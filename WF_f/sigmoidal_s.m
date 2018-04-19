743b3c4ba90f18f0068e9c62772631c8e428c307
Sara Jimenez <sarajcorrea@gmail.com>
2018-04-19 16:51:32 +0300

    Updated

--- WF_f/sigmoidal_s.m
+++ WF_f/sigmoidal_s.m
@@ -4,9 +4,15 @@ function dx = sigmoidal_s(t,x,param) % 12 parameters
     x2=x(2);
     
     % Sigmoid x1: a1=param(1), b1=param(2), c1=param(3).
-    s1=param(1)+(1-param(1))./(1+exp((-4*param(2)/param(3))*(x1-param(3))/(1-param(1))));
+    s1=param(1)+(1-param(1))./(1+exp((-4*param(2))*(x1-param(3))/(1-param(1))));
     % Sigmoid x2: a2=param(4), b2=param(5), c2=param(6).
-    s2=param(4)+(1-param(4))./(1+exp((-4*param(5)/param(6))*(x2-param(6))/(1-param(4))));
+    s2=param(4)+(1-param(4))./(1+exp((-4*param(5))*(x2-param(6))/(1-param(4))));
+    
+%     % Sigmoid x1: a1=param(1), b1=param(2), c1=param(3).
+%     s1=param(1)+1./(1+exp(-param(2)*(x1-param(3))));
+%     % Sigmoid x2: a2=param(4), b2=param(5), c2=param(6).
+%     s2=param(4)+1./(1+exp(-param(5)*(x2-param(6))));
+    
     % Input
     xI=0;
     
